27d82d29223ee2df6fe28ed80ae6d1097c0db00a
Thomas Laird <t6r6l5@gmail.com>
2017-10-27 10:29:54 -0600

    create component, import CSS

--- src/Author.js
+++ src/Author.js
@@ -1,8 +1,19 @@
 // Create an Author component
-<div className='story-author-info'>
-  <img src={props.authImg} className='story-author-image' />
-  <div className='story-author-text'>
-    <h4>{props.authName}</h4>
-    <h4>{props.estTime} read</h4>
-  </div>
-</div>
+import React, { Component } from 'react';
+import './Author.css'
+
+const Author = (props) => {
+  let newProps = props.props;
+  console.log(newProps);
+  return (
+    <div className='story-author-info'>
+      <img src={newProps.authImg} className='story-author-image' />
+      <div className='author-info-text'>
+        <h4 className="author-text">{newProps.authName}</h4>
+        <h4 className="author-text">{newProps.estTime} read</h4>
+      </div>
+    </div>    
+  )
+}
+
+export default Author;
