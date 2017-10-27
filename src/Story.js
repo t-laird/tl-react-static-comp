f9ef68e71221bb10b2a17c8ff3ba477a7f71177b
Thomas Laird <t6r6l5@gmail.com>
2017-10-27 10:32:30 -0600

    add props for each story

--- src/Story.js
+++ src/Story.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import Button from './Button';
 import './Story.css';
+import Author from './Author';
 
 const Story = (props) => {
+  let storyProps = props.props;
+  // console.log(storyProps);
   return (
     <div className='Story'>
-      <img src={props.img} />
+      <img src={storyProps.img} />
       <div className='story-info'>
         <div>
-          <h3>{props.title}</h3>
-          <p>{props.desc}</p>
+          <h3>{storyProps.title}</h3>
+          <p>{storyProps.desc}</p>
         </div>
+        <Author props={storyProps}/>
         {/* create an Author component and add it here */}
         {/* it will need properties of the author image, name, and the estimated reading time of the story */}
       </div>
