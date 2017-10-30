74476ec636b6206991e13d247029f01d72bec955
Thomas Laird <t6r6l5@gmail.com>
2017-10-29 21:50:54 -0600

    create second stories array and add photos

--- src/App.js
+++ src/App.js
@@ -7,33 +7,67 @@ import './App.css';
 
 const stories = [
   {
-    img: '../public/story1.jpg',
-    title: 'Winning at Fonts',
-    desc: 'or, Why you should care about typography',
+    img: '../public/stoory1.jpg',
+    title: 'So you\'re not his background wallpaper...',
+    desc: 'Who\'s this other dog and should you be worried?',
     authImg: '../public/louisa.png',
     authName: 'Louisa',
     estTime: '2 min'
   },
   {
-    img: '../public/story2.jpg',
-    title: 'Hoodlums: a dissertation',
-    desc: 'Examining villains in modern storytelling',
+    img: '../public/stoory2.jpeg',
+    title: 'Dog vacations 2017',
+    desc: 'What is this "beach" and do they have fetch there too?',
     authImg: '../public/alternate.png',
     authName: 'Nathaniel',
     estTime: '10 min'
   },
   {
-    img: '../public/story3.jpg',
-    title: 'Halloween is a year-round sport',
-    desc: 'Those who say otherwise just aren\'t trying hard enough.',
+    img: '../public/stoory3.jpg',
+    title: '"Don\'t you have enough photos of me?"',
+    desc: 'How to confront your human who spoils every moment with their camera.',
     authImg: '../public/leta.png',
     authName: 'Leta',
     estTime: '8 min'
   },
   {
-    img: '../public/story4.jpg',
-    title: 'How to Improve Learning and Classroom Engagement with Humor',
-    desc: 'Okay, so first of all, everybody knows fart jokes are hilarious ...',
+    img: '../public/stoory4.jpeg',
+    title: 'He said "Do you want to go for a walk?" 10 Minutes Ago',
+    desc: 'You\'ve been ready for 15. Is this thing happening or what?',
+    authImg: '../public/pamela.png',
+    authName: 'Pamela',
+    estTime: '11 min'
+  }
+]
+const stories2 = [
+  {
+    img: '../public/stoory5.jpg',
+    title: 'On a short leash?',
+    desc: 'Here are 5 helpful tips to get the freedom you need',
+    authImg: '../public/louisa.png',
+    authName: 'Louisa',
+    estTime: '2 min'
+  },
+  {
+    img: '../public/stoory6.png',
+    title: 'So you weren\'t the only Ariel at your weekend halloween party...',
+    desc: 'Get your creativity back!',
+    authImg: '../public/alternate.png',
+    authName: 'Nathaniel',
+    estTime: '10 min'
+  },
+  {
+    img: '../public/stoory7.jpeg',
+    title: 'Why can I sit at the table but not eat the table food?',  
+    desc: 'Helpful hints for deciphering mixed signals.',    
+    authImg: '../public/leta.png',
+    authName: 'Leta',
+    estTime: '8 min'
+  },
+  {
+    img: '../public/stoory8.jpg',
+    title: 'You know I don\'t like water',   
+    desc: 'So why would you put me on a paddle board?',   
     authImg: '../public/pamela.png',
     authName: 'Pamela',
     estTime: '11 min'
@@ -47,7 +81,7 @@ const App = () => {
       <Banner />
       <Stories  storiesTitle="Today's Top Stories"
                 stories={stories} />
-      <Stories storiesTitle="Pop Culture" stories={stories} />
+      <Stories storiesTitle="Pop Culture" stories={stories2} />
       <Footer />
     </div>
   );
